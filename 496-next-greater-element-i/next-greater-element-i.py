e778b30a25e20ed468bfdcbba2178837be214791
Rwirangira <142627846+P-Rwirangira@users.noreply.github.com>
2026-02-25 10:36:46 +0000

    Time: 3 ms (41.19%) | Memory: 19.4 MB (58.50%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,11 +1,11 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        hMap = defaultdict(lambda: -1)
+        freq = defaultdict(lambda: -1)
         stack = []
         for n in nums2:
             while stack and n > stack[-1]:
-                hMap[stack.pop()] = n
+                freq[stack.pop()] = n
             stack.append(n)
         
-        return [hMap[n] for n in nums1]
+        return [freq[n] for n in nums1]
         
